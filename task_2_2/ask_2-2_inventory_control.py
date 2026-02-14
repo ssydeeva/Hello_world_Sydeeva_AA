53d827d69b131d3508c487c1c84771431344d42f
ssydeeva <ssydeeva@icloud.com>
2026-02-14 18:34:04 +0300

    task 2_2

---
+++ task_2_2/ask_2-2_inventory_control.py
@@ -0,0 +1,3 @@
+reagent_name = input('введите название реактива')
+reagent_quantity = input('введите его количество в виде целого числа')
+print(f"Реактив {reagent_name} поступил на склад в количестве {reagent_quantity} шт.")
---
+++ task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+device_name = 'Микроскоп'
+device_number = '001'
+device_state = 'исправлен'
+device_quantity = "23"
+print("Название прибора\tИнвентарный номер\tСостояние\tКоличество")
+print(f'{device_name}\t\t{device_number}\t\t\t{device_state}\t{device_quantity}')
